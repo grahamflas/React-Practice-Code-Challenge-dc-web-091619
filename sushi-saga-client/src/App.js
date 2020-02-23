b1a74c201bfd5b22ca383d85c3892e60d50df3a9
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-23 15:22:20 -0500

    Fetch sushi

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,9 +6,34 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      allSushi: [],
+      wallet: 100,
+      servedSushi: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchSushi()
+  }
+
+  fetchSushi(){
+    fetch(API)
+      .then(response => response.json())
+      .then(sushiData => this.setState({
+        allSushi: sushiData
+      }))
+  }
+
+  serveSushi(){
+    let allSushiCopy = [...this.state.allSushi]
+    let newServedSushi = 
+  }
 
   render() {
-    return (
+    return ( 
       <div className="app">
         <SushiContainer  />
         <Table />
